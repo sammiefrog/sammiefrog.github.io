f111f76b6ab2139f6a06cb4430759ed474a531bd
sammiefrog <sammiefrog@live.com>
2020-05-29 15:03:11 -0500

    added projects and click events

--- js/main.js
+++ js/main.js
@@ -33,149 +33,180 @@ $(window).on('load', function() {
 
 
 (function($){
-
-	/*------------------
+              /*------------------
   		HEADER
   	--------------------*/
-	var navMenu = $('.menu-list')
-		navMenu.onePageNav();
-	$(window).on('scroll resize',function(e) {
-		if ($(this).scrollTop() > 70) {
-			$('.header-section').addClass('sticky');
-		}else{
-			$('.header-section').removeClass('sticky');
-		}
-		e.preventDefault();
-	});
+              var navMenu = $(".menu-list");
+              navMenu.onePageNav();
+              $(window).on("scroll resize", function (e) {
+                if ($(this).scrollTop() > 70) {
+                  $(".header-section").addClass("sticky");
+                } else {
+                  $(".header-section").removeClass("sticky");
+                }
+                e.preventDefault();
+              });
 
-	$('.responsive').on('click', function(event) {
-		$('.menu-list').slideToggle(400);
-		$('.header-section').toggleClass('bgc');
-		event.preventDefault();
-	});
+              $(".responsive").on("click", function (event) {
+                $(".menu-list").slideToggle(400);
+                $(".header-section").toggleClass("bgc");
+                event.preventDefault();
+              });
 
-	$('.menu-list li a').on('click', function(event) {
-		if ($(window).width() < 768) {
-			$('.menu-list').slideUp(400);
-			$('.header-section').removeClass('bgc');
-		}
-	});
+              $(".menu-list li a").on("click", function (event) {
+                if ($(window).width() < 768) {
+                  $(".menu-list").slideUp(400);
+                  $(".header-section").removeClass("bgc");
+                }
+              });
 
-
-
-	/*------------------
+              /*------------------
 		TYPED JS
 	--------------------*/
-	$(".element").typed({
-		strings: ["I'm Sammantha Sasenick", "Full Stack Web Developer", "From Chicago, IL"],
-		typeSpeed: 10,
-		loop:true,
-		backDelay: 2000
-	});
+              $(".element").typed({
+                strings: [
+                  "I'm Sammantha Sasenick",
+                  "Full Stack Web Developer",
+                  "From Chicago, IL",
+                ],
+                typeSpeed: 10,
+                loop: true,
+                backDelay: 2000,
+              });
 
-
-
-	/*------------------
+              /*------------------
 		FOOTER
 	--------------------*/
-	var fh = $('.footer-section').height();
-		fh = fh + 140;
-	$('.main-warp').css('margin-bottom', fh);
+              var fh = $(".footer-section").height();
+              fh = fh + 140;
+              $(".main-warp").css("margin-bottom", fh);
 
-	
-
-	/*------------------
+              /*------------------
 		PROGRESS BAR
 	--------------------*/
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>100%</span></div>');
-		}
-	});
-
-
+              $(".progress-bar-style").each(function () {
+                var progress = $(this).data("progress");
+                var prog_width = progress + "%";
+                if (progress <= 100) {
+                  $(this).append(
+                    '<div class="bar-inner" style="width:' +
+                      prog_width +
+                      '"><span>' +
+                      prog_width +
+                      "</span></div>"
+                  );
+                } else {
+                  $(this).append(
+                    '<div class="bar-inner" style="width:100%"><span>100%</span></div>'
+                  );
+                }
+              });
 
 	/*------------------
+	RECENT PROJECTS
+	--------------------*/
+	$('.view1').on('click', function (event) {
+		event.preventDefault();
+		window.open("https://github.com/sammiefrog/track-your-employees");
+	});
+
+		$(".view2").on("click", function (event) {
+      event.preventDefault();
+      window.open("https://github.com/sammiefrog/theHungryMonkey");
+		});
+	
+		$(".view3").on("click", function (event) {
+      event.preventDefault();
+      window.open("https://github.com/sammiefrog/eat-da-burger");
+		});
+	
+		$(".view4").on("click", function (event) {
+      event.preventDefault();
+      window.open("https://github.com/sammiefrog/rules");
+		});
+	
+		$(".view5").on("click", function (event) {
+      event.preventDefault();
+      window.open("https://github.com/sammiefrog/readme-generator");
+		});
+	
+		$(".view6").on("click", function (event) {
+      event.preventDefault();
+      window.open("https://github.com/sammiefrog/emp-template-generator");
+    });
+
+    /*------------------
 		OWL CAROUSEL
 	--------------------*/
-	$('#review-carousel').owlCarousel({
-		dots: false,
-		nav: true,
-		loop: true,
-		margin:30,
-		smartSpeed: 700,
-		items:1,
-		autoplay:true,
-		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
-	});
+              $("#review-carousel").owlCarousel({
+                dots: false,
+                nav: true,
+                loop: true,
+                margin: 30,
+                smartSpeed: 700,
+                items: 1,
+                autoplay: true,
+                navText: [
+                  '<i class="fa fa-angle-left"></i>',
+                  '<i class="fa fa-angle-right"></i>',
+                ],
+              });
 
-
-
-	/*------------------
+              /*------------------
 		MAGNIDIC POPUP
 	--------------------*/
-	$('.work-item').magnificPopup({
-		type: 'image',
-		gallery: { enabled: true },
-		removalDelay: 400,
-		zoom:{enabled: true, duration: 300}
-	});
+              $(".work-item").magnificPopup({
+                type: "image",
+                gallery: { enabled: true },
+                removalDelay: 400,
+                zoom: { enabled: true, duration: 300 },
+              });
 
-
-	/*------------------
+              /*------------------
 		WOW JS
 	--------------------*/
-	new WOW().init();
+              new WOW().init();
 
-
-	/*------------------
+              /*------------------
 		CONTACT FORM
 	--------------------*/
-	$('#contact-form').on('submit', function() {
-		var send_btn = $('#send-form'),
-			form = $(this),
-			formdata = $(this).serialize(),
-			chack = $('#form-chack');
-			send_btn.text('Wait...');
+              $("#contact-form").on("submit", function () {
+                var send_btn = $("#send-form"),
+                  form = $(this),
+                  formdata = $(this).serialize(),
+                  chack = $("#form-chack");
+                send_btn.text("Wait...");
 
-		function reset_form(){
-		 	$("#name").val('');
-			$("#email").val('');
-			$("#massage").val('');
-		}
+                function reset_form() {
+                  $("#name").val("");
+                  $("#email").val("");
+                  $("#massage").val("");
+                }
 
-		$.ajax({
-			url:  $(form).attr('action'),
-			type: 'POST',
-			data: formdata,
-			success : function(text){
-				if (text == "success"){
-					send_btn.addClass('done');
-					send_btn.text('Success');
-					setTimeout(function() {
-						reset_form();
-						send_btn.removeClass('done');
-						send_btn.text('Massage');
-					}, 2500);
-				}
-				else {
-					reset_form();
-					send_btn.addClass('error');
-					send_btn.text('Error');
-					setTimeout(function() {
-						send_btn.removeClass('error');
-						send_btn.text('Massage');
-					}, 5000);
-				}
-			}
-		});
-		return false;
-	});
-
-
-})(jQuery);
+                $.ajax({
+                  url: $(form).attr("action"),
+                  type: "POST",
+                  data: formdata,
+                  success: function (text) {
+                    if (text == "success") {
+                      send_btn.addClass("done");
+                      send_btn.text("Success");
+                      setTimeout(function () {
+                        reset_form();
+                        send_btn.removeClass("done");
+                        send_btn.text("Massage");
+                      }, 2500);
+                    } else {
+                      reset_form();
+                      send_btn.addClass("error");
+                      send_btn.text("Error");
+                      setTimeout(function () {
+                        send_btn.removeClass("error");
+                        send_btn.text("Massage");
+                      }, 5000);
+                    }
+                  },
+                });
+                return false;
+              });
+            })(jQuery);
