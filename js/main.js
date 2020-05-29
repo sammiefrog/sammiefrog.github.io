2ac8f46bc4c53514d857055a0adfe6226276286b
sammiefrog <sammiefrog@live.com>
2020-05-29 18:03:16 -0500

    updated footer icons, working on contact form

--- js/main.js
+++ js/main.js
@@ -194,7 +194,7 @@ $(window).on('load', function() {
                       setTimeout(function () {
                         reset_form();
                         send_btn.removeClass("done");
-                        send_btn.text("Massage");
+                        send_btn.text("Message");
                       }, 2500);
                     } else {
                       reset_form();
@@ -202,7 +202,7 @@ $(window).on('load', function() {
                       send_btn.text("Error");
                       setTimeout(function () {
                         send_btn.removeClass("error");
-                        send_btn.text("Massage");
+                        send_btn.text("Message");
                       }, 5000);
                     }
                   },
--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@
 		
 			// Set the recipient email address.
 	        // Type here your email id for receive email.
-	        $email_to = "wwfbcom@bootex.com"; 
+	        $email_to = "ssasenick412@gmail.com"; 
 
 	        // Set the email subject.
 	        $subject = "New email from $name";
